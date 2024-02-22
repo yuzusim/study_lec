a3f788573ded4db0b1cf7458a4cb6a41e46d8979
getinthere <getinthere@naver.com>
2024-02-22 11:42:41 +0900

    삭제하기 ajax

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +16,19 @@ import java.util.List;
 @RestController
 public class BoardApiController {
     private final BoardRepository boardRepository;
+
+    @DeleteMapping("/api/boards/{id}")
+    public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response){
+        Board board = boardRepository.selectOne(id);
+        if(board == null){
+            response.setStatus(404);
+            return new ApiUtil<>(404, "해당 게시글을 찾을 수 없습니다");
+        }
+
+        boardRepository.deleteById(id);
+        return new ApiUtil<>(null);
+    }
+
 
     @GetMapping("/api/boards")
     public ApiUtil<?> findAll(HttpServletResponse response) {
