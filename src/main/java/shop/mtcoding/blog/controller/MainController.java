d8e40b822b717f7b3b68e5ae74a4e1209fc6dafa
chugue <chugue85@gmail.com>
2024-02-28 19:36:23 +0900

    repetive code deleted

--- src/main/java/shop/mtcoding/blog/controller/MainController.java
+++ src/main/java/shop/mtcoding/blog/controller/MainController.java
@@ -11,4 +11,33 @@ public class MainController {
     public String main () {
         return "index";
     }
+
+    @GetMapping("/jobs/interest")
+    public String interest () {
+        return "/jobs/interest";
+    }
+    @GetMapping("/resume/manageResume")
+    public String manageResume () {
+        return "/resume/manageResume";
+    }
+
+    @GetMapping("/jobs/info")
+    public String info () {
+        return "/jobs/info";
+    }
+    @GetMapping("/user/loginForm")
+    public String loginForm () {
+        return "/user/loginForm";
+    }
+    @GetMapping("/user/joinForm")
+    public String joinForm () {
+        return "/user/joinForm";
+    }
+    @GetMapping("/comp/loginForm")
+    public String compLogin () {
+        return "/comp/loginForm";
+    }
+
+
+
 }
