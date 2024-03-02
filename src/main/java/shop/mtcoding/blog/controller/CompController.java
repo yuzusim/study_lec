cc08de1c878c27dedf026f6b68007e729e712796
AI_Nomads <chugue85@gmail.com>
2024-03-02 16:47:55 +0900

    join still dummydata needed

--- src/main/java/shop/mtcoding/blog/controller/CompController.java
+++ src/main/java/shop/mtcoding/blog/controller/CompController.java
@@ -10,27 +10,29 @@ import shop.mtcoding.blog.dto.user.UserRequest;
 import shop.mtcoding.blog.model.comp.CompRepository;
 import shop.mtcoding.blog.model.jobs.Jobs;
 import shop.mtcoding.blog.model.jobs.JobsRepository;
+import shop.mtcoding.blog.model.user.User;
+import shop.mtcoding.blog.model.user.UserRepository;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class CompController {
-
-    private  final CompRepository compRepository;
-    private  final HttpSession session;
+    private final UserRepository userRepository;
+    private final CompRepository compRepository;
+    private final HttpSession session;
 
     @GetMapping("/comp/apply")
-    public String apply(){
+    public String apply() {
         return "/comp/apply";
     }
 
     @GetMapping("/comp/comphome/{compId}")
-    public String compHome(@PathVariable Integer compId){
+    public String compHome(@PathVariable Integer compId) {
         //comp-id값으로 공고리스트를 가져오고  배열
         List<Jobs> jobsList = compRepository.findAll(compId);
 
-        session.setAttribute("jobList",jobsList);
+        session.setAttribute("jobList", jobsList);
 
         //리스트를 담는 ArrayList 생성
         //세션에 저장
@@ -39,49 +41,56 @@ public class CompController {
         return "/comp/comphome";
     }
 
-    @PostMapping("/comp/joinForm")
-    public String compJoinForm(UserRequest.UserAllDTO requestDTO){
-        System.out.println(requestDTO.getRole());
+    @GetMapping("/comp/joinForm")
+    public String compJoinForm() {
+
         return "/comp/joinForm";
     }
 
-    @GetMapping("/comp/loginForm")
-    public String compLoginForm(){
-        return "/comp/loginForm";
+    @PostMapping("/comp/join/{role}")
+    public String compJoin(@PathVariable int role, UserRequest.UserAllDTO requestDTO){
+        requestDTO.setRole(role);
+        userRepository.save(requestDTO);
+        List<User> userList = userRepository.findAll();
+        System.out.println(userList);
+        return "redirect:/";
     }
 
+
+
     @GetMapping("/comp/profileUpdateForm")
-    public String profileUpdateForm(){
+    public String profileUpdateForm() {
         return "/comp/profileUpdateForm";
     }
 
     @GetMapping("/comp/readResume")
-    public String readResume(){
+    public String readResume() {
         return "/comp/readResume";
     }
 
     @GetMapping("/comp/scrap")
-    public String scrap(){
+    public String scrap() {
         return "/comp/scrap";
     }
 
     @GetMapping("/comp/talent")
-    public String talent(){
+    public String talent() {
         return "/comp/talent";
     }
 
     @GetMapping("/comp/updateForm")
-    public String updateForm(){
+    public String updateForm() {
         return "/comp/updateForm";
     }
 
     @GetMapping("/comp/jobsInfo")
-    public String jobsInfo(){
+    public String jobsInfo() {
 
         return "/comp/jobsInfo";
     }
+
     @GetMapping("/comp/compResumeDetail")
-    public String compResumeDetail(){
+    public String compResumeDetail() {
         return "/comp/compResumeDetail";
     }
 }
